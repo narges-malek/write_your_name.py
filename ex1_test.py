b65a228bd15ae403f31af63ce65178cc9f13b89f
narges-malek <106173610+narges-malek@users.noreply.github.com>
2024-05-18 23:57:50 -0400

    Delete ex1_test.py

--- ex1_test.py
+++
@@ -1,9 +0,0 @@
-import write_your_name as wyn
-
-def test_hi_my_name_is():
-    result = wyn.hi_my_name_is()
-    print("Function output:", result)
-    assert len(result)  > 1
-
-
-test_hi_my_name_is()
