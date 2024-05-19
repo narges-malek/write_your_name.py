cf6bf047a45a2a88ae2d1a9505d9ea5acf3976df
narges-malek <106173610+narges-malek@users.noreply.github.com>
2024-05-19 03:13:38 -0400

    Update ex1_test.py

--- ex1_test.py
+++ ex1_test.py
@@ -1,5 +1,8 @@
 import write_your_name as name_point
 
 def test_hi_my_name_is():
+    result = wyn.hi_my_name_is()
+    print("Function output:", result)
     assert len(name_point.hi_my_name_is())  > 1 
 
+test_hi_my_name_is()
