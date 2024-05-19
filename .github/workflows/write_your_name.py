246b55570051618ea5a9fc8cf65ebc8779173a9e
narges-malek <106173610+narges-malek@users.noreply.github.com>
2024-05-19 18:24:32 -0400

    Add files via upload

---
+++ .github/workflows/write_your_name.py
@@ -0,0 +1,11 @@
+def hi_my_name_is():
+    """
+    This function returns the name of the user.
+    
+    Returns:
+        str: The name of the user.
+    """
+    return "Narges Malek"
+
+if __name__ == "__main__":
+    print(hi_my_name_is())
